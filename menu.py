23715741f458675b399e11d0d01b2bdc07bb264b
valentina27101 <pachecovalentinap@gmail.com>
2026-03-20 19:54:02 -0500

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,7 @@
+# Esta función solo muestra el menú de opciones
+def mostrar_menu():
+    print("===MENÚ===")
+    print("1. Agregar Producto")
+    print("2. Mostrar Inventario")
+    print("3. Calcular Estadisticas")
+    print("4. Salir")
