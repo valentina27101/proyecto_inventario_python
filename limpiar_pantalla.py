6f52ded9174b5f121aa67b9ac4750eaa48666aac
valentina27101 <pachecovalentinap@gmail.com>
2026-03-20 20:40:24 -0500

    Create limpiar_pantalla.py

---
+++ limpiar_pantalla.py
@@ -0,0 +1,15 @@
+from os import system
+import sys
+
+
+def deleteScreen():
+    if sys.platform == "linux" or sys.platform == "darwin":
+        system("clear")
+    else:
+        system("cls")
+
+def pauseScreen():
+    if sys.platform == "linux" or sys.platform == "darwin":
+        pause = input("Presione una enter para continuar...")
+    else:
+        system("pause")
