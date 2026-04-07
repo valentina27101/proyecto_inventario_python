99bf62db136c3ebd3a16c68f3c324e0f853ea1f4
valentina27101 <pachecovalentinap@gmail.com>
2026-04-06 23:16:29 -0500

    Add menu display function to menu.py

---
+++ menu.py
@@ -0,0 +1,26 @@
+# Esta función solo muestra el menú de opciones
+def mostrar_menu():
+    
+    print( "--------------------------")
+    print("        ===MENÚ===")
+    print("--------------------------")
+    print("   1. Agregar Producto")
+    print("--------------------------")
+    print( "  2. Mostrar Inventario")
+    print("--------------------------")
+    print("   3. Buscar Productos")
+    print("--------------------------")
+    print("  4. Actualizar Producto")
+    print("--------------------------")
+    print("   5. Eliminar Producto")
+    print("--------------------------")
+    print("6. Calculo De Estadisticas")
+    print("--------------------------")
+    print("     7. Guardar CSV")
+    print("--------------------------")
+    print("      8. Cargar CSV")
+    print("--------------------------")
+    print("        9. Salir")
+    print("--------------------------")
+
+   
